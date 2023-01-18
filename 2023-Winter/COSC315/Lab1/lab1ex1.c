60a51f51dcbd6e81a44efa498327ffeaa71e8ac2
Kevin Ngkaion <kevinngkaion@gmail.com>
2023-01-17 23:15:31 -0800

    Started Lab 1 for 315. lab1ex1.c is my first C program in class

---
+++ 2023-Winter/COSC315/Lab1/lab1ex1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int main(){
+    int input = 0;
+    int min = INT_MAX;
+    int max = 0;
+    int sum = 0;
+    int count = 0;
+    float average = 0;
+    int variance = 0;
+
+    //loop until user provides stop input
+    while (input != -1){
+        //prompt user for input
+        printf("Enter a positive integer (-1 to stop): ");
+        scanf("%d", &input);
+        //only execute this if user has not input exit code
+        if (input != -1){
+            if (input < min){
+                min = input;
+            }
+            if (input > max){
+                max = input;
+            }
+            sum += input;
+            count++;
+        }
+    }
+
+    //Calculate the average
+    average = (double)sum/count;
+
+    printf("The sum of %d integers is \n", count);
+    printf("The Maximum: %5d \n", max);
+    printf("The Minimum: %5d \n", min);
+    printf("The Average: %6.3f \n", average);
+}
