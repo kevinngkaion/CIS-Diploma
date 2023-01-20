d7992dc466740874600a5c723d5ef33997832876
Kevin Ngkaion <kevinngkaion@gmail.com>
2023-01-19 16:23:22 -0800

    Completed exercise2.

--- 2023-Winter/COSC315/Lab1/lab1ex2.c
+++ 2023-Winter/COSC315/Lab1/lab1ex2.c
@@ -1,15 +1,12 @@
 /*
 Author: Kevin Ngkaion
 Date: Jan 18, 2023
-Description: This program takes integers from the user as input and calculates the
-             average, min, max, and variance of the numbers entered.
+Description: This program takes the first name and last name from the user as well as a lucky number.
+             It then creates a username by taking first char of first name and concatenating the last
+             name and lucky number to it before returning the username and lucky number to the user.
 COSC315 LAB 1
 */
-//TODO: Create username for person:
-//      1. Get first char in fname
-/*      2. Concat to lname
-        3. Concat favnumber
-*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -20,6 +17,7 @@ int main(){
     char* username;
     int lucky = getUserInfo(&username);
     printf("The lucky number of user %s is %d.\n", username, lucky);
+    free(username);
     return 0;
 }
 
@@ -29,8 +27,20 @@ int getUserInfo(char** uname){
     int num;
     printf("Enter your first and last name: ");
     scanf("%s %s", fname, lname);
-    printf("Hello %s. Enter your lucky number (from 1 to 99):", fname);
+    printf("Hello %s. Enter your lucky number (from 1 to 99): ", fname);
     scanf("%d", &num);
-    *uname = fname;
+
+    //allocate memory for the username
+    *uname = (char*)malloc(64*sizeof(char));
+    //Set first char of uname to first char of fname
+    **uname = *fname;
+    //Concat lname to uname
+    strcat(*uname, lname);
+    char* lucknum = (char*)malloc(8*sizeof(char));
+    sprintf(lucknum, "%d", num);
+    strcat(*uname, lucknum);
+
+    free(fname);
+    free(lname);
     return num;
 }
